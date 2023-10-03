d8a1610c0007d205d5378bb3791a0c4b3f82801e
Mamounalhtita <302734233@student.rocmondriaan.nl>
2023-10-03 16:00:48 +0200

    ..
    klaar

---
+++ main.js
@@ -0,0 +1,54 @@
+//opdracht 1
+console.log("main.js loaded...");
+
+let numbers = [5,4,3,8,2,3];
+console.log("numbers is: ",numbers);
+
+//opdracht 2
+
+// let fruit = ["appel", "peer", "banaan", "kiwi", "perzik"];
+// console.log(fruit);
+
+
+
+// opdr. 1
+let x = [4, 5, 8, 12, -1, 99];
+console.log("opdr 1:", x);
+
+// opdr. 2: 
+let m = [0.33, 4.8, 11.1, 34.5, 129.1, -2.56, -17.4];
+console.log("opdr 2:", m);
+
+//opdr 3
+let l = ['rood', 'groen', 'geel', 'blauw', 'roze'];
+console.log("opdr 3:", l);
+
+//opdr 4
+x[3] = "21";
+console.log("opdr 4:",x);
+
+//opdr 5
+m[0] = "3.3";
+console.log("opdr 5:",m);
+
+//opdr5
+m[6] = "-1.74";
+console.log("opdr 5:", m);
+
+//opdr 6
+l[3] = "paars";
+console.log("opdr 6:", l);
+
+//opdr 7 
+l.push("zwart");
+console.log("opdr 7:", l);
+
+//opdr 8
+x.unshift(0);
+console.log("opdr 8:", x);
+
+//opdr 9
+m.shift();
+console.log("opdr 9:",m);
+m.pop();
+console.log("opdr 9:", m);
